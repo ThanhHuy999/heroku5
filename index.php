4bd1bf37e216a28aad1b4a99955346e06f36430f
ThanhHuy999 <67851389+ThanhHuy999@users.noreply.github.com>
2022-03-29 12:24:01 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+		<meta charset="utf-8">
+		<title>B1809579 - Bach Van Thanh Huy</title>
+		<meta name="generator" content="Bootply" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+		<!--[if lt IE 9]>
+			<script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+		<link href="css/styles.css" rel="stylesheet">
+	</head>
+	<body>
+<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+	<div class="navbar-header">
+		
+
+</nav>
+
+<div class="container-fluid">
+  
+  <!--left-->
+
+     
+ <!--/left-->
+  
+  <!--center-->
+  <div class="col-sm-9">
+    <div class="row">
+      <div class="col-xs-12">
+        <h3>Ma so sinh vien: B1809579</h3>
+		<h3>Ho va ten: Bach Van Thanh Huy</h3>
+ <hr>
+<h4>Xin chao den voi website cua tui tren Heroku!</h4>
+		
+	Welcome to Heroku!<?php include("counter.php");?>
+      </div>
+    </div>
+    <hr>
+	
+        
+   
+  </div><!--/center-->
+
+  <!--right-->
+  <div class="col-sm-3">
+
+    	<div class="panel panel-default">
+       
+         	<div class="panel-body"></div>
+        </div>
+   
+  </div>
+<!--/right-->
+  <hr>
+  <h4> Thong tin lien he:</h4>
+  <h4> Phone: 0353112343 </h4>
+  <h4> Email: huyb1809579@student.ctu.edu.vn</h4>
+  
+</div><!--/container-fluid-->
+	<!-- script references -->
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+	</body>
+</html>
